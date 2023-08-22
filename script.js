0f2e23bbca810b9eff3ec1124f738f91d4d043e4
Paladin333777 <romavolvyn@gmail.com>
2023-08-22 19:02:39 -0400

    Change render functuonality.

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ const Gameboard = (() => {
     let gameboard = ['', '', '', '', '', '', '', '', '']
 
     const render = () => {
+        const board = document.getElementById('game')
+        let boardCell
+        gameboard.forEach((cell, index) => {
+            boardCell = document.createElement('div')
+            boardCell.setAttribute('id', `${index}`)
+            boardCell.setAttribute('class', 'cell')
+            boardCell.innerHTML = `${cell}`
+            board.appendChild(boardCell)
+        })
         const cells = document.querySelectorAll('.cell')
-        const handleClick = (event) => {
-            let index = parseInt(event.target.id)
-            console.log(index)
-        }
-        cells.forEach((cell) => {
-            cell.addEventListener('click', handleClick){
-                Game.update()
-            }
+        cells.forEach((unit) => {
+            unit.addEventListener('click', Game.unitClick)
         })
     }
+
+    const update = (index, value) => {
+        gameboard[index] = value
+        console.log(gameboard)
+    }
+
+    const getGameboard = () => gameboard;
     return {
-        render
+        render, getGameboard, update
     }
 })();
 
@@ -23,18 +33,21 @@ const Game = (() => {
     let playerIndex = 0
     let gameActive;
 
-    const update = () => {
+    const unitClick = (event) => {
+        let index = event.target.id
+        if(event.target.innerHTML !== '') {
+            return
+        } else {
+            event.target.innerHTML = players[playerIndex]
+        }
+        Gameboard.update(index, players[playerIndex])
 
+        playerIndex = playerIndex === 0 ? 1 : 0;
     }
-
-    playerIndex = playerIndex === 0 ? 1 : 0;
 
     return {
-        update
+        unitClick
     }
 })();
-
-
-
 
 Gameboard.render()
